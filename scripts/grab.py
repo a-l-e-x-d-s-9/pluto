b1d9be92c88c439797fbcab344a580b9001f4c83
dina <dinasv@post.bgu.ac.il>
2015-12-13 16:35:49 +0200

    added Grip.srv grip.py

---
+++ scripts/grab.py
@@ -0,0 +1,71 @@
+ 
+#!/usr/bin/env python
+#Made by Bohan Lou Email: loubohan@gmail.com
+import rospy
+from pluto.srv import Grip
+from std_msgs.msg import Float64
+from std_msgs.msg import String
+from dynamixel_msgs.msg import JointState as dxl_JointState
+ 
+def lf_callback(data):
+	global msg 
+	msg[0] = data.load * -1
+	msg[2] = data.current_pos
+ 
+def rf_callback(data):
+	global msg
+	msg[1] = data.load
+	msg[3] = data.current_pos
+ 
+def handle_gripper(req):
+	global msg
+	global pub_left_finger
+	global pub_right_finger
+	msg = [0,0,0,0]
+ 
+ 
+	if req.Option == "Close":
+		while msg[1] < req.Value or msg[0] < req.Value:
+			msg[2] += 0.05
+			msg[3] -= 0.05
+			lf = Float64(msg[2]) 	
+			rf = Float64(msg[3])
+			rospy.loginfo('left_finger_'+str(lf))
+			rospy.loginfo('right_finger_'+str(rf))
+			rospy.loginfo('left_load_'+str(msg[0]))
+			rospy.loginfo('right_load_'+str(msg[1]))
+			pub_left_finger.publish(lf)
+			pub_right_finger.publish(rf)
+			rospy.sleep(0.3)
+ 
+		return msg[1]
+ 
+	elif req.Option == "Open":
+ 
+		lf = Float64(req.Value) 	
+		rf = Float64(req.Value * -1)
+		rospy.loginfo('left_finger_'+str(lf))
+		rospy.loginfo('right_finger_'+str(rf))
+		pub_left_finger.publish(lf)
+		pub_right_finger.publish(rf)
+		rospy.sleep(0.5)
+ 
+		return msg[2]
+ 
+def gripper_server():
+		#print "Options: Open Close \n Angle Values: -1.20~0.8 \n Load Values: 0.00 ~ 0.15"
+		global msg
+		msg = [0,0,0,0]
+		rospy.init_node('gripper_server')
+		global pub_left_finger
+		global pub_right_finger
+		pub_left_finger = rospy.Publisher('/komodo_1/left_finger_controller/command', Float64, queue_size=10)
+		pub_right_finger = rospy.Publisher('/komodo_1/right_finger_controller/command', Float64, queue_size=10)
+		rospy.Subscriber('/komodo_1/left_finger_controller/state', dxl_JointState, lf_callback)
+		rospy.Subscriber('/komodo_1/right_finger_controller/state', dxl_JointState, rf_callback)
+		s = rospy.Service('gripper', Grip, handle_gripper)
+		rospy.spin()
+ 
+ 
+if __name__ == '__main__':
+	gripper_server()
