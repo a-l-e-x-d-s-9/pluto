a2ff158c1f5517f8f0fe867b899296639b8359a4
dina <dinasv@post.bgu.ac.il>
2015-12-24 16:45:24 +0200

    changed movit.py got move it to work

--- scripts/movit.py
+++ scripts/movit.py
@@ -60,9 +60,47 @@ def move_group_python_interface_tutorial():
   rospy.sleep(10)
   print "============ Starting tutorial "
 
-  moveit_commander.roscpp_shutdown()
+  print "============ Reference frame: %s" % group.get_planning_frame()
+  print "============ Reference frame: %s" % group.get_end_effector_link()
+  print "============ Robot Groups:"
+  print robot.get_group_names()
+  print "============ Printing robot state"
+  print robot.get_current_state()
+  print "============"
+  
+  print "============ Generating plan 1"
+  pose_target = geometry_msgs.msg.Pose()
+
+  pose_target.orientation.w = 1.0;
+  pose_target.orientation.x = 0.0;
+  pose_target.orientation.y = 0.0;
+  pose_target.orientation.z = 0.0;
+  pose_target.position.x = 0.5;
+  pose_target.position.y = 0.0;
+  pose_target.position.z = 0.5;
+  group.set_pose_target(pose_target)
+  group.set_goal_tolerance(0.31);
+  plan1 = group.plan()
+
+  print "============ Waiting while RVIZ displays plan1..."
+  rospy.sleep(5)
+  
+  print "============ Visualizing plan1"
+  display_trajectory = moveit_msgs.msg.DisplayTrajectory()
+
+  display_trajectory.trajectory_start = robot.get_current_state()
+  display_trajectory.trajectory.append(plan1)
+  display_trajectory_publisher.publish(display_trajectory);
+
+  print "============ Waiting while plan1 is visualized (again)..."
+  rospy.sleep(5)
+  group.go(wait=True)
+  
 if __name__=='__main__':
+  
   try:
+    rospy.init_node("movit")
     move_group_python_interface_tutorial()
+    rospy.spin()
   except rospy.ROSInterruptException:
     pass
